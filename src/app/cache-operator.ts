df2b4e5d9a9407d03236e91f4ca728359394ea17
alain-armis <alain@armis.tech>
2019-05-09 14:41:10 +0200

    use switchMapTo

--- src/app/cache-operator.ts
+++ src/app/cache-operator.ts
@@ -7,11 +7,12 @@ export interface CacheOperatorConfig {
 }
 
 class ResettableSubject<T = any> extends Observable<T> implements Observer<T> {
-  private resettableSubject: Subject<T>;
-  private resetter = new ReplaySubject<void>(1);
+  private resettableSubject$: Subject<T>;
+  private resetter$ = new ReplaySubject<void>(1);
 
   // source comes from Observable class
-  source = this.resetter.pipe(switchMap(() => this.resettableSubject));
+  // specify that the source is the inner resettableSubject rebuild by the factory after each resetter$ event
+  source = this.resetter$.pipe(switchMap(() => this.resettableSubject$));
 
   constructor(private subjectFactory: () => Subject<T> = () => new ReplaySubject(1)) {
     super();
@@ -19,18 +20,18 @@ class ResettableSubject<T = any> extends Observable<T> implements Observer<T> {
   }
 
   // Observer implementation
-  next(value?: T) { this.resettableSubject.next(value); }
-  error(err: any) { this.resettableSubject.error(err); }
+  next(value?: T) { this.resettableSubject$.next(value); }
+  error(err: any) { this.resettableSubject$.error(err); }
   complete() {
-    this.resetter.complete();
-    this.resettableSubject.complete();
+    this.resetter$.complete();
+    this.resettableSubject$.complete();
   }
 
   reset() { this.resetSubject(); }
 
   private resetSubject() {
-    this.resettableSubject = this.subjectFactory();
-    this.resetter.next();
+    this.resettableSubject$ = this.subjectFactory();
+    this.resetter$.next();
   }
 }
 
@@ -42,7 +43,7 @@ export const cache = <T>({ expiration, clear$ }: CacheOperatorConfig = {}): Mono
 
   fetchData$.pipe(
     tap(() => fetchingData = true),
-    switchMap(() => source$.pipe(catchError(error => {
+    switchMapTo(source$.pipe(catchError(error => {
       fetchingData = false;
       cache$.error(error);
       cache$.reset();
@@ -53,7 +54,7 @@ export const cache = <T>({ expiration, clear$ }: CacheOperatorConfig = {}): Mono
       fetchingData = false;
       expiredData = false;
     }),
-    switchMap(() => race(...[expiration && timer(expiration), clear$ && clear$.pipe(take(1))].filter(Boolean))),
+    switchMapTo(race(...[expiration && timer(expiration), clear$ && clear$.pipe(take(1))].filter(Boolean))),
     tap(() => {
       expiredData = true;
       cache$.reset();
