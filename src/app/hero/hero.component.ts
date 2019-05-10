ebaffa2e219423f414c284b69328894c75175613
Alain P <alain.paolini291@gmail.com>
2019-05-10 08:44:00 +0200

    f

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
-import { retryWhen, retry } from 'rxjs/operators';
+import { retryWhen, retry, tap } from 'rxjs/operators';
 
 import { HeroService, Hero } from '../hero.service';
 
 @Component({
   selector: 'app-hero',
   template: `
-    <!--{{ heroService.hero$ | async | json }}-->
-    <ng-container *ngIf="hero">{{ hero | json }}</ng-container>
-    <ng-container *ngIf="error">error</ng-container>
+    {{ hero$ | async | json }}
+    <!--<ng-container *ngIf="hero">{{ hero | json }}</ng-container>
+    <ng-container *ngIf="error">error</ng-container>-->
     <!--<button (click)="retry$.next()">retry</button>-->
   `,
   styles: []
@@ -18,15 +18,16 @@ export class HeroComponent {
   hero: Hero;
   error = false;
   // retry$ = new Subject<void>();
+  hero$ = this.heroService.hero$.pipe(tap(() => console.log('hero component')));
 
   constructor(public heroService: HeroService) {
-    heroService.hero$.pipe(
-      // retryWhen(() => this.retry$)
-      // retry(3)
-    ).subscribe(
-      hero => { this.error = false; this.hero = hero; console.log('component', hero); },
-      err => { this.error = true; console.log('component', err); },
-      () => console.log('component complete')
-    );
+    // heroService.hero$.pipe(
+    //   // retryWhen(() => this.retry$)
+    //   // retry(3)
+    // ).subscribe(
+    //   hero => { this.error = false; this.hero = hero; console.log('component', hero); },
+    //   err => { this.error = true; console.log('component', err); },
+    //   () => console.log('component complete')
+    // );
   }
 }
