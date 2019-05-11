82dd0bb4cd515945ce65f24a76b8d72c2f8a21ae
Alain P <alain.paolini291@gmail.com>
2019-05-11 21:23:37 +0200

    remove resettablesubject

--- src/app/cache-operator.ts
+++ src/app/cache-operator.ts
@@ -1,76 +1,53 @@
-import { Observable, Observer, Subject, ReplaySubject, MonoTypeOperatorFunction, race, timer, defer, EMPTY, NEVER } from 'rxjs';
-import { switchMap, switchMapTo, tap, finalize, take, catchError } from 'rxjs/operators';
+import { Observable, Subject, ReplaySubject, MonoTypeOperatorFunction, Subscription, defer, race, timer } from 'rxjs';
+import { switchMapTo, switchMap, take, tap, takeWhile } from 'rxjs/operators';
 
 export interface CacheOperatorConfig {
   expiration?: number;
   clear$?: Observable<any>;
 }
 
-//TODO : voir https://github.com/ReactiveX/rxjs/blob/master/src/internal/operators/shareReplay.ts
-
-export class ResettableSubject<T = any> extends Observable<T> implements Observer<T> {
-  private resettableSubject$: Subject<T>;
-  private resetter$ = new ReplaySubject<void>(1);
-
-  // source comes from Observable class
-  // specify that the source is the inner resettableSubject rebuild by the factory after each resetter$ event
-  source = this.resetter$.pipe(switchMap(() => this.resettableSubject$));
-
-  constructor(private subjectFactory: () => Subject<T> = () => new ReplaySubject(1)) {
-    super();
-    this.resetSubject();
-  }
-
-  // Observer implementation
-  next(value?: T) { this.resettableSubject$.next(value); }
-  error(err: any) { this.resettableSubject$.error(err); }
-  complete() {
-    this.resetter$.complete();
-    this.resettableSubject$.complete();
-  }
-
-  reset() { this.resetSubject(); }
-
-  private resetSubject() {
-    this.resettableSubject$ = this.subjectFactory();
-    this.resetter$.next();
-  }
-}
-
 export const cache = <T>({ expiration, clear$ }: CacheOperatorConfig = {}): MonoTypeOperatorFunction<T> => (source$: Observable<T>) => {
-  const cache$ = new ResettableSubject<T>();
-  let expiredData = true;
-  let fetchingData = false;
-  const fetchData$ = new Subject<void>();
+  let cache$: ReplaySubject<T>;
+  const startClear$ = new Subject<void>();
+  const resetter$ = new ReplaySubject<void>(1);
+  let expired = true;
+  let fetching = false;
+  let hasError = false;
+  let subscription: Subscription;
 
-  fetchData$.pipe(
-    tap(() => fetchingData = true),
-    switchMap(() => source$.pipe(
-      catchError(error => {
-        fetchingData = false;
-        expiredData = true;
-        cache$.error(error);
-        cache$.reset();
-        return NEVER;
-      }),
-      // finalize(() => cache$.complete())
-    )),
-    tap(data => {
-      cache$.next(data);
-      fetchingData = false;
-      expiredData = false;
-    }),
-    switchMap(() => race(...[expiration && timer(expiration), clear$ && clear$.pipe(take(1))].filter(Boolean))),
-    tap(() => {
-      expiredData = true;
-      cache$.reset();
-    })
-  ).subscribe();
+  startClear$.pipe(
+    tap(() => expired = false),
+    takeWhile(() => !!(expiration || clear$)),
+    switchMapTo(
+      race(...[expiration && timer(expiration), clear$ && clear$.pipe(take(1))].filter(Boolean))
+    )
+  ).subscribe(() => expired = true);
 
   return defer(() => {
-    if (!fetchingData && expiredData) {
-      fetchData$.next();
+    if (!fetching && expired || hasError) {
+      if (subscription) { subscription.unsubscribe(); }
+      cache$ = new ReplaySubject(1);
+      resetter$.next();
+      fetching = true;
+      hasError = false;
+
+      subscription = source$.subscribe({
+        next(value) {
+          fetching = false;
+          startClear$.next();
+          cache$.next(value);
+        },
+        error(err) {
+          fetching = false;
+          hasError = true;
+          cache$.error(err);
+        },
+        complete() {
+          cache$.complete();
+        }
+      });
     }
-    return cache$;
+
+    return resetter$.pipe(switchMap(() => cache$));
   });
 };
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { of, Subject, merge, ReplaySubject, noop, throwError, timer, iif, BehaviorSubject } from 'rxjs';
-
-// import { cache } from './cache-operator';
-import { cache } from './cache-operator2';
 import { tap, switchMap, delay, take, switchMapTo, shareReplay } from 'rxjs/operators';
+
+import { cache } from './cache-operator';
 
 export interface Hero {
   name: string;
@@ -27,41 +26,19 @@ export class HeroService {
     this.errorSuccess$.next(true);
   });
 
-  private readonly heroDB$ = of(noop()).pipe(
-    switchMapTo(iif(() => this.errorSuccess$.value, this.heroDBSuccess$, this.heroDBError$)),
-    delay(500)
-  );
-
-  // private readonly heroDB$ = this.heroDBSuccess$.pipe(delay(500));
+  // private readonly heroDB$ = of(noop()).pipe(
+  //   switchMapTo(iif(() => this.errorSuccess$.value, this.heroDBSuccess$, this.heroDBError$)),
+  //   delay(500)
+  // );
+  private readonly heroDB$ = this.heroDBSuccess$.pipe(delay(500));
   // private readonly heroDB$ = this.heroDBError$;
 
   readonly hero$ = merge(
     this.heroDB$,
-    this.updateHero$/*.pipe(tap(() => console.log('from update')))*/,
+    this.updateHero$.pipe(tap(() => console.log('from update'))),
   ).pipe(
-    // cache({ expiration: 1000 })
-    // cache({ clear$: this.clearHeroCache$ })
-    // shareReplay()
     cache({ expiration: 1000, clear$: this.clearHeroCache$ })
   );
-
-  // test = this.updateHero$.subscribe(
-  //   v => console.log('updateHero$ next', v),
-  //   err => console.log('updateHero$ error', err),
-  //   () => console.log('updateHero$ complete')
-  // );
-
-  // merge = this.hero$.subscribe(
-  //   v => console.log('merge next', v),
-  //   err => console.log('merge error', err),
-  //   () => console.log('merge complete')
-  // );
-
-  // merge2 = timer(1100).pipe(switchMapTo(this.hero$)).subscribe(
-  //   v => console.log('merge2 next', v),
-  //   err => console.log('merge2 error', err),
-  //   () => console.log('merge2 complete')
-  // );
 
   updateHero(hero: Hero) {
     this.hero = hero;
